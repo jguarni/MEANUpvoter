7595b16d9e2249ea1af2f942bfb8d7e038f3435e
jguarni <JoeGuarni@Joes-MacBook-Pro.local>
2015-04-03 21:44:42 -0400

    added routes

---
+++ routes/auth.js
@@ -0,0 +1,22 @@
+var router = require("express").Router();
+var passport = require("passport");
+
+router.get('/github',
+  passport.authenticate('github'),
+  function(req, res){
+    // The request will be redirected to GitHub for authentication, so this
+    // function will not be called.
+  });
+
+router.get('/github/callback',
+  passport.authenticate('github', { failureRedirect: '/' }),
+  function(req, res) {
+    res.redirect('/');
+  });
+
+router.get('/logout', function(req, res){
+  req.logout();
+  res.redirect('/');
+});
+
+module.exports = router;
